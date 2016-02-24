7f3335e244344b9fa005c7e5ce2a2295290e6231
tonyyanxuan <tony0620emma@yahoo.com.tw>
2016-02-24 13:09:32 +0800

    upload calculate.c
    this file is to calculate the result of the phonebook program
    then gnuplot can plot it

---
+++ calculate.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    FILE *fp = fopen("orig.txt", "r");
+    FILE *output = fopen("output.txt", "w");
+    int i = 0;
+    char append[50], find[50];
+    double orig_sum_a = 0.0, orig_sum_f = 0.0, orig_a, orig_f;
+    for (i = 0; i < 100; i++) {
+        fscanf(fp, "%s %s %lf %lf\n", append, find,&orig_a, &orig_f);
+        orig_sum_a += orig_a;
+        orig_sum_f += orig_f;
+    }
+
+    fp = fopen("opt.txt", "r");
+    double opt_sum_a = 0.0, opt_sum_f = 0.0, opt_a, opt_f;
+    for (i = 0; i < 100; i++) {
+        fscanf(fp, "%s %s %lf %lf\n", append, find,&opt_a, &opt_f);
+        opt_sum_a += opt_a;
+        opt_sum_f += opt_f;
+    }
+    fprintf(output, "append() %lf %lf\n",orig_sum_a / 100.0, opt_sum_a / 100.0);
+    fprintf(output, "findName() %lf %lf", orig_sum_f / 100.0, opt_sum_f / 100.0);
+    fclose(output);
+    fclose(fp);
+    return 0;
+}
